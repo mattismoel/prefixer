992c71a10e6b1ce06f73359b8e183e872c259394
Mattis Kristensen <Mattis Ml Kristensen>
2023-09-09 14:26:31 +0200

    Added placeholder tutorial page

---
+++ src/routes/tutorial/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<div class="flex-1">
+	<h1 class="text-xl font-semibold mb-4">What is Prefixer?</h1>
+	<p class="mb-8">
+		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus corrupti, vitae unde
+		molestiae facere dolorum voluptatibus delectus aliquam similique facilis odio non et explicabo
+		consequuntur, dolores ut, in tempore sed. Sequi omnis quibusdam assumenda, in aspernatur
+		nesciunt blanditiis. Excepturi laudantium dolor saepe voluptate voluptas voluptatem. Ut repellat
+		qui corporis quisquam ratione debitis architecto rerum ea nisi, eum animi fugiat excepturi!
+		Possimus, recusandae tempora.
+	</p>
+	<h1 class="text-xl font-semibold mb-4">How do i play?</h1>
+	<p>
+		Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit quisquam, totam voluptatum
+		eius rerum odit odio dolores laudantium possimus. Molestias omnis praesentium quam rem amet aut
+		quibusdam, illum hic vitae! Dicta veniam laboriosam fuga odit aut vel quasi. Accusantium culpa
+		iusto assumenda excepturi cupiditate repellendus exercitationem alias. Doloribus dicta obcaecati
+		tempora aut at id eaque veniam voluptates minus. Neque, tenetur. Id animi impedit praesentium at
+		quisquam consequuntur, molestiae ab odio eveniet aperiam saepe eius nam harum, corporis optio
+		ad. Saepe ipsum eos ad totam voluptates sequi rem voluptate quasi vero? Iusto earum, cum quas
+		incidunt facere ex necessitatibus recusandae eligendi. Recusandae repellat doloremque culpa
+		molestias laborum blanditiis ipsam nostrum debitis sapiente illo cupiditate necessitatibus,
+		rerum iusto quaerat nihil. Voluptates, debitis.
+	</p>
+</div>
